b9dd91d6e4ebb96e58b9a6ab5e09a2b0eca6f21a
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-04-07 09:48:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$entryfee
+$age = 45;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+if (($age <= 15 && $age => 65)) {
+    $entryfee = 50;
+}else {
+    $entryfee = 100;
+}
+?>
+
+<p> Je vám <?= $age ?> Musíte zaplatit <?= $entryfee ?> Korun. </p>
+
+</body>
+</html>
